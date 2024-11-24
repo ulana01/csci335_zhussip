92a9791dcb8ea2c85c60c0b1570c6c1c7dc369ff
Ulana <ulanazhusip@gmail.com>
2024-11-25 00:26:52 +0500

    feat: added lab 11

--- week6/BittyProcessor/obj_dir/register_tb.d
+++
@@ -1,7 +0,0 @@
-register_tb.o: ../test/register_tb.cpp \
-  /opt/homebrew/Cellar/verilator/5.028/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.028/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.028/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.028/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.028/share/verilator/include/verilated_funcs.h \
-  Vregister.h
--- week6/Lab11/obj_dir/mux_tb.d
+++
@@ -1,7 +0,0 @@
-mux_tb.o: ../test/mux_tb.cpp \
-  /opt/homebrew/Cellar/verilator/5.028/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.028/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.028/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.028/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.028/share/verilator/include/verilated_funcs.h \
-  Vmux.h
